4d02ef75c59acb78df5fda4ed1a125e283875769
SteveR <136388557+stevie912@users.noreply.github.com>
2025-06-17 20:19:33 +0000

    Added database credentials to config.php

--- app/core/config.php
+++ app/core/config.php
@@ -14,8 +14,14 @@ define('LOGS', ROOT . DS . 'logs');
 define('FILES', ROOT . DS. 'files');
 
 // ---------------------  NEW DATABASE TABLE -------------------------
-define('DB_HOST',         'xxw.h.filess.io');
-define('DB_USER',         '4806_sentenceby'); 
-define('DB_PASS',         $_ENV['DB_PASS']);
-define('DB_DATABASE',     '4806_sentenceby');
-define('DB_PORT',         '3305');
+// define('DB_HOST',         'xxw.h.filess.io');
+// define('DB_USER',         '4806_sentenceby'); 
+// define('DB_PASS',         $_ENV['DB_PASS']);
+// define('DB_DATABASE',     '4806_sentenceby');
+// define('DB_PORT',         '3305');
+
+define('DB_HOST', 'r2okq.h.filess.io');
+define('DB_USER', 'cosc4806_plentynone');
+define('DB_PASS', $_ENV['DB_PASS']);
+define('DB_DATABASE', 'cosc4806_plentynone');  
+define('DB_PORT', '3305');
