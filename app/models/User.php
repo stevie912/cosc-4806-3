d0ceb2134cf4c9bc15bcf483a7fe1794745b3a8c
SteveR <136388557+stevie912@users.noreply.github.com>
2025-06-19 16:44:39 +0000

    moved my login validation to user.php model

--- app/models/User.php
+++ app/models/User.php
@@ -18,33 +18,69 @@ class User {
       return $rows;
     }
 
-    public function authenticate($username, $password) {
+    public function authenticate($username, $password)   {
         /*
          * if username and password good then
          * $this->auth = true;
          */
 		$username = strtolower($username);
-		$db = db_connect();
-        $statement = $db->prepare("select * from users WHERE username = :name;");
-        $statement->bindValue(':name', $username);
-        $statement->execute();
-        $rows = $statement->fetch(PDO::FETCH_ASSOC);
-		
-		if (password_verify($password, $rows['password'])) {
-			$_SESSION['auth'] = 1;
-			$_SESSION['username'] = ucwords($username);
-			unset($_SESSION['failedAuth']);
-			header('Location: /home');
-			die;
-		} else {
-			if(isset($_SESSION['failedAuth'])) {
-				$_SESSION['failedAuth'] ++; //increment
-			} else {
-				$_SESSION['failedAuth'] = 1;
-			}
-			header('Location: /login');
-			die;
-		}
-    }
 
+      $db = db_connect();
+      //check user is in database
+      $statement = $db->prepare("SELECT * FROM users WHERE username = ?");
+      $statement->execute([$username]);
+
+      if ($statement->rowCount() == 0) {  //user not found
+        $_SESSION['no_user'] = true;
+        header("Location: /login");
+
+      }
+
+      else {
+        //get hashed password from database
+        $statement = $db->prepare("SELECT password FROM users WHERE username = :username");
+        $statement->bindParam(':username', $username);
+        $statement->execute();
+        $statement->bindColumn('password', $valid_password);
+        $$valid_password = $statement->fetch(PDO::FETCH_BOUND);
+
+        //check if password is correct
+        if (password_verify($password, $valid_password)) {
+          $_SESSION['authenticated'] = true;
+          header("Location: /home");
+        } else {
+          if (!isset($_SESSION['failed_attempts'])) {
+            $_SESSION['failed_attempts'] = 1;
+          } else {
+            $_SESSION['failed_attempts'] += 1;
+          }
+
+          header("Location: /login");
+
+        }
+      }
+    }
 }
+      
+		// $db = db_connect();
+  //       $statement = $db->prepare("select * from users WHERE username = :name;");
+  //       $statement->bindValue(':name', $username);
+  //       $statement->execute();
+  //       $rows = $statement->fetch(PDO::FETCH_ASSOC);
+		
+		// if (password_verify($password, $rows['password'])) {
+		// 	$_SESSION['auth'] = 1;
+		// 	$_SESSION['username'] = ucwords($username);
+		// 	unset($_SESSION['failedAuth']);
+		// 	header('Location: /home');
+		// 	die;
+		// } else {
+		// 	if(isset($_SESSION['failedAuth'])) {
+		// 		$_SESSION['failedAuth'] ++; //increment
+		// 	} else {
+		// 		$_SESSION['failedAuth'] = 1;
+		// 	}
+		// 	header('Location: /login');
+		// 	die;
+		// }
+
