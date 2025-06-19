79b126b8bf0a39eb5506f7eca97b6f89ea1e8367
SteveR <136388557+stevie912@users.noreply.github.com>
2025-06-19 19:33:54 +0000

    added error message to database.php

--- app/database.php
+++ app/database.php
@@ -9,6 +9,7 @@ function db_connect() {
         $dbh = new PDO('mysql:host=' . DB_HOST . ';port='. DB_PORT . ';dbname=' . DB_DATABASE, DB_USER, DB_PASS);
         return $dbh;
     } catch (PDOException $e) {
+        echo "Error connecting to database: " . $e->getMessage();
         //We should set a global variable here so we know the DB is down
     }
 }
