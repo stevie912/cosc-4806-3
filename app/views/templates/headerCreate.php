b09c8c95c54fdac2e09bc1c75bccf3212a4b8969
SteveR <136388557+stevie912@users.noreply.github.com>
2025-07-04 14:33:05 +0000

    updated bootstrap version in headers and JS in footer

--- app/views/templates/headerCreate.php
+++ app/views/templates/headerCreate.php
@@ -16,7 +16,8 @@ if (isset($_SESSION['pass_insecure'])) {
 
 <!DOCTYPE html>
 <html lang="en">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+
     <link rel="icon" href="/favicon.png">
     <title>Create new user</title>
     <meta charset="UTF-8">
